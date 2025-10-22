eaf89a6bf243d1ea863aad91a36383badbc35e7a
shiqinx <sjy1592023@outlook.com>
2025-10-22 17:02:42 +0800

    添加 campus-activity 后端文件

---
+++ campus-activity-backend/src/main/java/com/fluffy/campus_activity/CampusActivityApplication.java
@@ -0,0 +1,13 @@
+package com.fluffy.campus_activity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusActivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusActivityApplication.class, args);
+	}
+
+}
---
+++ campus-activity-backend/src/test/java/com/fluffy/campus_activity/CampusActivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fluffy.campus_activity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusActivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
