eaf20cc16e35c0cc757ab0c62034d755343469ab
shiqinx <sjy1592023@outlook.com>
2025-10-23 12:25:48 +0800

    添加campus-activity-backend后端

---
+++ activity-campus-backend/src/main/java/com/myteam/activity_campus_backend/ActivityCampusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.myteam.activity_campus_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityCampusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivityCampusBackendApplication.class, args);
+	}
+
+}
---
+++ activity-campus-backend/src/test/java/com/myteam/activity_campus_backend/ActivityCampusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myteam.activity_campus_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivityCampusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
